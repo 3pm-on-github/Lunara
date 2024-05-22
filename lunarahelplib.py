a20a2b96de77423bb780fd4485be34889647b470
GachaYTB <pookiemon2019@gmail.com>
2024-05-22 20:21:47 +0200

    big update

--- lunarahelplib.py
+++
@@ -1,9 +0,0 @@
-# made this library to help devs program lunara
-
-class LunaraHelpLib():
-    def isanint(self, thing):
-        try:
-            int(thing)
-            return True
-        except:
-            return False
